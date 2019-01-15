f08d68d1e470715fe625702cd69940b55d4700b2
Thomas AKKARI <thomas.akkari@gmail.com>
2019-01-15 15:36:37 +0100

    Added PLS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,29 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MapComponent } from './map/map.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { FormComponent } from './form/form.component';
+import { ToolboxComponent } from './toolbox/toolbox.component';
+
+import{ FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    RechercheComponent,
+    FormComponent,
+    ToolboxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    LeafletModule.forRoot()
+    LeafletModule.forRoot(),
+    FontAwesomeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -33,6 +33,21 @@ export class MapComponent implements OnInit {
         })
     ])
     
+    private centralesLayerGroup = L.layerGroup([
+        circle([ 42.25, 9.03 ], { radius: 1000, color: "#FFB800"}),
+        circle([ 42.17, 9.26 ], { radius: 1000, color: "#FFB800" }),
+        circle([ 42.42, 9.02 ], { radius: 1000, color: "#FFB800" }),
+        circle([ 42.34, 9.46 ], { radius: 1000, color: "#CC0000" }),
+        circle([ 42.56, 9.1 ], { radius: 1000 , color: "#00FF12"}),
+        circle([ 41.97, 8.96 ], { radius: 1000 , color: "#00FF12" }),
+        circle([ 42.17, 8.76 ], { radius: 1000 , color: "#00FF12"}),
+        circle([ 41.87, 9.06 ], { radius: 1000 , color: "#00FF12"}),
+        circle([ 41.77, 9.06 ], { radius: 1000 , color: "#00FF12"}),
+        circle([ 41.77, 9.26 ], { radius: 1000 , color: "#00FF12"}),
+        circle([ 41.67, 9.36 ], { radius: 1000 , color: "#00FF12"}),
+        circle([ 41.57, 9.0 ], { radius: 1000,  color: "#CC0000"  })
+        
+    ])
     constructor() { }
 
     ngOnInit() {
@@ -56,21 +71,10 @@ export class MapComponent implements OnInit {
     
     
     layersControl = {
-        baseLayers: {
-            'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-            'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-        },
         overlays: {
-            'Big Circle': circle([ 42.17, 9.06 ], { radius: 5000 }),
-            'Big Square': polygon([[ 42.1, 9.1 ], [ 46.9, 10.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]]),
-            'Marker': marker([42.17, 9.06],{
-            icon: L.icon({
-              iconSize: [ 25, 41 ],
-              iconAnchor: [ 13, 41 ],
-              iconUrl: this.markerIconUrl
-                })   
-            }),
-            'Test' : this.centralLayerGroup
+            'Centrales': this.centralesLayerGroup,
+            'Post-Sources': polygon([[ 42.1, 9.1 ], [ 46.9, 10.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]]),
+            
         }
     }
 
